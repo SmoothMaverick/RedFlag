85f52f4a6a6b6ab94ba2fba273be2d9ffd7b706e
jason <jasonhoang7@gmail.com>
2014-02-16 06:35:29 -0800

    fix uid

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,8 @@ class Company
   include Neo4j::ActiveNode
 
   property :name, type: String
-  property :uid, type: String
+  property :uid, type: Integer
+
 
   has_n(:competitors).to("Company")
 
--- app/models/duns_server.rb
+++ app/models/duns_server.rb
@@ -4,10 +4,10 @@ class DunsServer
 		#query company name and company id
 		response = HTTParty.get("http://dnbdirect-api.dnb.com/DnBAPI-13/rest/search/company/#{term}?postBodyContentType=application%2Fjson", :headers => { "Username" => "api3322", "Password" => "welcome", "Api-Key" => "m8z88pw4899qgt8uv7w7779g"})
 	
-		cName  = [response["resultSet"]["hit"].first["companyResults"]["companyName"]]	
-		cId = response["resultSet"]["hit"].first["companyResults"]["companyId"]
+		cName  = response["resultSet"]["hit"].first["companyResults"]["companyName"]	
+		cId  = response["resultSet"]["hit"].first["companyResults"]["companyId"]	
 		
-		Company.create(name:cName,uuid:cId)
+		Company.create(name:cName,uid:cId)
 	
 		#query company news
 
