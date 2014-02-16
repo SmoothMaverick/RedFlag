0558eb06cc7c78a45fc0231946f7f64f35f8e14d
jason <jasonhoang7@gmail.com>
2014-02-15 21:19:09 -0800

    add company name and parent name to results

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,20 @@
 class ApiController < ApplicationController
 	def company_search
-		response = HTTParty.get("http://dnbdirect-api.dnb.com/DnBAPI-13/rest/search/company/pepsi?postBodyContentType=application%2Fjson", :headers => { "Username" => "api3322", "Password" => "welcome", "Api-Key" => "m8z88pw4899qgt8uv7w7779g"})
-		render :json => ["company1", "company2", "company3"]
+		puts params[:term]	
+		
+		response = HTTParty.get("http://dnbdirect-api.dnb.com/DnBAPI-13/rest/search/company/#{params[:term]}?postBodyContentType=application%2Fjson", :headers => { "Username" => "api3322", "Password" => "welcome", "Api-Key" => "m8z88pw4899qgt8uv7w7779g"})
+	
+		cName  = [response["resultSet"]["hit"].first["companyResults"]["companyName"]]	
+		pDuns = response["resultSet"]["hit"].first["companyResults"]["ultimateParentDuns"]
 
+		pResponse = HTTParty.get("http://dnbdirect-api.dnb.com/DnBAPI-13/rest/company/#{pDuns}?view=simple&postBodyContentType=application%2Fjson", :headers => { "Username" => "api3322", "Password" => "welcome", "Api-Key" => "m8z88pw4899qgt8uv7w7779g"})
+
+		pName  = [pResponse["name"]]
+		#render :json => [cName, pResponse]
+
+		render :json => { :company_name => cName, :parent_name => pName }
+
+		# render :json => [response["resultSet"]["hit"].first["companyResults"]["companyName"], [response["resultSet"]["hit"].first["companyResults"]["ultimateParentDuns"]]	
 	end
 end
 
