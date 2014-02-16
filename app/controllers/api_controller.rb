90272025d14c0438daf3a76b649d4990a78c9ab9
jason <jasonhoang7@gmail.com>
2014-02-16 06:26:21 -0800

    fix models

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,7 @@
 class ApiController < ApplicationController
 	def company_search
-    # 
-    #DunsServer.company_search params[:term]
-
-		#render :json => { :company_name => cName, :parent_name => pName }
+    company = DunsServer.company_search params[:term]
+		render :json => company
 
 	end
 end
--- app/models/duns_server.rb
+++ app/models/duns_server.rb
@@ -1,10 +1,19 @@
 class DunsServer
-  def company_search(term)
+	include Neo4j::ActiveNode
+  def self.company_search(term)
+		#query company name and company id
 		response = HTTParty.get("http://dnbdirect-api.dnb.com/DnBAPI-13/rest/search/company/#{term}?postBodyContentType=application%2Fjson", :headers => { "Username" => "api3322", "Password" => "welcome", "Api-Key" => "m8z88pw4899qgt8uv7w7779g"})
 	
 		cName  = [response["resultSet"]["hit"].first["companyResults"]["companyName"]]	
 		cId = response["resultSet"]["hit"].first["companyResults"]["companyId"]
 		
 		Company.create(name:cName,uuid:cId)
+	
+		#query company news
+
+		#query competititors
+
+		#query competitor news
+
   end
 end
