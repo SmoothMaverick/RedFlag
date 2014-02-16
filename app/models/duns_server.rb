c7d0b53d94a1793d270b7e5e5a2ee834b25964a2
Michael Rapadas <mike@mrap.me>
2014-02-16 07:30:59 -0800

    Converts tabs to spaces

--- app/models/duns_server.rb
+++ app/models/duns_server.rb
@@ -1,37 +1,38 @@
 class DunsServer
-	include Neo4j::ActiveNode
+  include Neo4j::ActiveNode
   def self.company_search(term)
-		#query company name and company id
-		response = HTTParty.get("http://dnbdirect-api.dnb.com/DnBAPI-13/rest/search/company/#{term}?postBodyContentType=application%2Fjson", :headers => { "Username" => "api3322", "Password" => "welcome", "Api-Key" => "m8z88pw4899qgt8uv7w7779g"})
-	
-		cName  = response["resultSet"]["hit"].first["companyResults"]["companyName"]	
-		cId  = response["resultSet"]["hit"].first["companyResults"]["companyId"]	
-		
-		Company.create(name:cName,uid:cId)
-	
-		#query company news
-		
+    #query company name and company id
+    response = HTTParty.get("http://dnbdirect-api.dnb.com/DnBAPI-13/rest/search/company/#{term}?postBodyContentType=application%2Fjson", :headers => { "Username" => "api3322", "Password" => "welcome", "Api-Key" => "m8z88pw4899qgt8uv7w7779g"})
 
-		#query competititors
+    cName  = response["resultSet"]["hit"].first["companyResults"]["companyName"]	
+    cId  = response["resultSet"]["hit"].first["companyResults"]["companyId"]	
 
-		#query competitor news
+    Company.create(name:cName,uid:cId)
+
+    #query company news
+
+
+    #query competititors
+
+    #query competitor news
 
   end
   def self.news_search(company)
-		#query company news
-		response = HTTParty.get("http://dnbdirect-api.dnb.com/DnBAPI-13/rest/company/#{company.uid}/news?start_date=2014-01-01&end_date=2014-02-01&filter=GeneralIndustry", :headers => { "Username" => "api3322", "Password" => "welcome", "Api-Key" => "m8z88pw4899qgt8uv7w7779g"})
+    #query company news
+    response = HTTParty.get("http://dnbdirect-api.dnb.com/DnBAPI-13/rest/company/#{company.uid}/news?start_date=2014-01-01&end_date=2014-02-01&filter=GeneralIndustry", :headers => { "Username" => "api3322", "Password" => "welcome", "Api-Key" => "m8z88pw4899qgt8uv7w7779g"})
 
-		newsArray  = response["companyNews"]["newsItems"]["newsItem"]	
-	
-		newsArray[0..20].each do |item|
-			news = News.new
-			news.title = item["title"]
-			news.text = item["text"]
-			news.source = item["source"]
-			news.link = item["link"]
-			news.date = item["date"]
-			news.save
-			news.companies << company
-		end
-	end
+    newsArray  = response["companyNews"]["newsItems"]["newsItem"]	
+
+    newsArray[0..20].each do |item|
+      news = News.new
+      news.title = item["title"]
+      news.text = item["text"]
+      news.source = item["source"]
+      news.link = item["link"]
+      news.date = item["date"]
+      if news.save
+        news.companies << company
+      end
+    end
+  end
 end
