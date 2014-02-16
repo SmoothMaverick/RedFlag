a0e17641e053a5449e7b9dff9d88a65098725636
Michael Rapadas <mike@mrap.me>
2014-02-16 13:15:09 -0800

    Adds search view

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,5 @@
+class CompaniesController < ApplicationController
+  def search
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 RedFlag::Application.routes.draw do
+
+root :to => 'companies#search'
 get 'company_search/:term' => 'api#company_search'
+post 'company_search' => 'api#company_search'
+get 'search' => 'companies#search'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
